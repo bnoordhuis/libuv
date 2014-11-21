bnoordhuis/libuv

commit d8de4fbd13f9284737a01dea32639785780c6f0a

build: compile with -fwrapv in debug mode

common.gypi
@@ -15,7 +15,7 @@
     'configurations': {
       'Debug': {
         'defines': [ 'DEBUG', '_DEBUG' ],
-        'cflags': [ '-g', '-O0' ],
+        'cflags': [ '-g', '-O0', '-fwrapv' ],
         'msvs_settings': {
           'VCCLCompilerTool': {
             'target_conditions': [

commit 50100d5abd6bb642cfb2a9cb943dd4ccc3b8a541

build: compile with -ftrapv in debug mode
Fixes a two year old bug that was introduced in commit d8de4fbd where
I accidentally introduced -fwrapv instead of -ftrapv.  The goal was to
catch signed integer overflow bugs at run-time but that is not what
-fwrapv does; it makes signed overflow have well-defined wraparound
behavior.  Mea culpa.

common.gypi
@@ -12,7 +12,7 @@
     'configurations': {
       'Debug': {
         'defines': [ 'DEBUG', '_DEBUG' ],
-        'cflags': [ '-g', '-O0', '-fwrapv', '-fvisibility=hidden' ],
+        'cflags': [ '-g', '-O0', '-ftrapv', '-fvisibility=hidden' ],
         'msvs_settings': {
           'VCCLCompilerTool': {
             'target_conditions': [